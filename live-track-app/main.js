d500bf766d38f4d4ca31b84dd6e3f2d18adca15f
jole84 <johanlennartsson84@gmail.com>
2023-03-16 21:54:47 +0100

    live-track add wmts and confirmation

--- live-track-app/main.js
+++ live-track-app/main.js
@@ -122,14 +122,31 @@ var ortofoto = new TileLayer({
   visible:false
 });
 
+// var topoweb = new TileLayer({
+//   source: new TileWMS({
+//     url: 'https://minkarta.lantmateriet.se/map/topowebb',
+//     params: {
+//       'layers': 'topowebbkartan',
+//       'TILED': true,
+//       'TRANSPARENT': false,
+//     },
+//   }),
+//   visible: false
+// });
+
+import WMTS from 'ol/source/WMTS.js';
+import WMTSTileGrid from 'ol/tilegrid/WMTS.js';
 var topoweb = new TileLayer({
-  source: new TileWMS({
-    url: 'https://minkarta.lantmateriet.se/map/topowebb',
-    params: {
-      'layers': 'topowebbkartan',
-      'TILED': true,
-      'TRANSPARENT': false,
-    },
+  source: new WMTS({
+    url: 'https://minkarta.lantmateriet.se/map/topowebbcache',
+    layer: 'topowebb',
+    format: 'image/png',
+    matrixSet: "3857",
+    tileGrid: new WMTSTileGrid({
+      origin: [-20037508.342789, 20037508.342789],
+      resolutions: [156543.03392804097, 78271.51696402048, 39135.75848201024, 19567.87924100512, 9783.93962050256, 4891.96981025128, 2445.98490512564, 1222.99245256282, 611.49622628141, 305.748113140705, 152.8740565703525, 76.43702828517625, 38.21851414258813, 19.109257071294063, 9.554628535647032, 4.777314267823516, 2.388657133911758, 1.194328566955879],
+      matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+    }),
   }),
   visible: false
 });
